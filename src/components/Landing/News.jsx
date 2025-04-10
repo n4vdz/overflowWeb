e07786f6e2d54a6909cc879962f86fb3d87ba3d6
n4vdz <navidhosseini.q@gmail.com>
2025-04-10 13:02:00 +0330

    .fix

--- src/components/Landing/News.jsx
+++ src/components/Landing/News.jsx
@@ -26,7 +26,7 @@ function News() {
       </div>
 
       <div className="space-y-6">
-        {newsData.map((newsItem,index) => (
+        {newsData&&newsData.map((newsItem,index) => (
           <div key={index} className="bg-white shadow-lg rounded-lg p-6">
             <div className="flex justify-between items-center">
               <div className="w-full md:w-1/2">
